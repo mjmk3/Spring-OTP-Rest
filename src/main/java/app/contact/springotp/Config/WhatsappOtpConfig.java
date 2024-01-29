32439d9d508065d0cf124f07f0cd68df21463772
Mohamad J Makki <mohamad.j.makki@gmail.com>
2024-01-29 14:45:46 +0300

    Updating configurations

---
+++ src/main/java/app/contact/springotp/Config/WhatsappOtpConfig.java
@@ -0,0 +1,12 @@
+package app.contact.springotp.Config;
+
+/**
+ * @author MJ Makki
+ * @version 1.0
+ * @license SkyLimits, LLC (<a href="https://www.skylimits.tech">SkyLimits, LLC</a>)
+ * @email m.makki@skylimits.tech
+ * @since long time ago
+ */
+
+public class WhatsappOtpConfig {
+}
